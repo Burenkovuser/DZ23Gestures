d7cf8348f58bff06424b4c4df7b7f35bc5bbd715
Burenkovuser <nb2207@yandex.ru>
2017-01-23 21:40:03 +0600

    tapGesture

--- DZ23 Gestures/ViewController.m
+++ DZ23 Gestures/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIGestureRecognizerDelegate>
 
+@property(weak,nonatomic) UIImageView *imageView;
 
 @end
 
@@ -24,9 +25,22 @@
     
     imageView.animationDuration = 1/2.f;
     [imageView startAnimating];
+    
+    imageView.autoresizingMask=  UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|
+    UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin;
    
     [self.view addSubview:imageView];
+    self.imageView = imageView; //без этого не двигалось
     
+    
+#pragma mark - Gestures
+
+    
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                               action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:tapGesture];
+    
+    tapGesture.delegate = self;
   
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -37,5 +51,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) handleTap :(UITapGestureRecognizer*) tapGesture {
+    CGPoint pointCenter = [tapGesture locationInView:self.view];
+    [UIView animateWithDuration:0.3
+     
+                        animations:^{
+                            //self.imageView.center = [tapGesture locationInView:self.view];
+                            self.imageView.center = pointCenter;
+                        }];
+    
+
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
+}
 
 @end
