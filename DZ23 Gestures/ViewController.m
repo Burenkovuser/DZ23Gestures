500b739ba14ba7d8c9f657d1fcce4c52c53d5620
Burenkovuser <nb2207@yandex.ru>
2017-01-24 13:34:13 +0600

    handleDoubleTapDoubleTouch

--- DZ23 Gestures/ViewController.m
+++ DZ23 Gestures/ViewController.m
@@ -30,17 +30,36 @@
     UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleBottomMargin;
    
     [self.view addSubview:imageView];
-    self.imageView = imageView; //без этого не двигалось
+    self.imageView = imageView; //без этого не было движения
     
     
-#pragma mark - Gestures
-
-    
+    //чтобы передвигать в место куда сделан клик
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self
                                                                                action:@selector(handleTap:)];
     [self.view addGestureRecognizer:tapGesture];
     
     tapGesture.delegate = self;
+    
+    //двойной клик (убирает картинку - останавливает анимацию)
+    
+    UITapGestureRecognizer *doubleTapGuesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleDoubleTap:)];
+    doubleTapGuesture.numberOfTapsRequired = 2;
+    
+    [self.view addGestureRecognizer:doubleTapGuesture];
+    
+    
+   UITapGestureRecognizer *doubleTapTouchGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:
+                                                    @selector(handleDoubleTapDoubleTouch:)];
+    doubleTapTouchGesture.numberOfTapsRequired = 2; //два клика
+    doubleTapTouchGesture.numberOfTouchesRequired = 2; //два тача
+    
+    [self.view addGestureRecognizer:doubleTapTouchGesture];
+    
+    UISwipeGestureRecognizer *rightSwipeGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleRighSwipeGesture:)];
+    
+    rightSwipeGesture.direction = UISwipeGestureRecognizerDirectionRight;
+    [self.view addGestureRecognizer:rightSwipeGesture];
+    
   
     // Do any additional setup after loading the view, typically from a nib.
 }
@@ -50,19 +69,58 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - Gestures
 
+//реализация метода передвижения за тэпом
 -(void) handleTap :(UITapGestureRecognizer*) tapGesture {
     CGPoint pointCenter = [tapGesture locationInView:self.view];
     [UIView animateWithDuration:0.3
-     
-                        animations:^{
-                            //self.imageView.center = [tapGesture locationInView:self.view];
+                            delay:0 options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionBeginFromCurrentState
+                            animations:^{
                             self.imageView.center = pointCenter;
-                        }];
+                            }
+     completion:^(BOOL finished) {
+         
+                 }];
+}
+// прерываем анимацию по двойному клику(имя селектора и имя объекта)?? как сделать чтобы анимированая картинка не останваливалась??
+-(void)handleDoubleTap:(UITapGestureRecognizer*) doubleTapGuesture {
+    self.imageView.center = self.view.center;
+    [self.imageView stopAnimating]; //for key останавливать по ключу
+}
+//двойной клик и два тача переводит картинку в центр
+-(void) handleDoubleTapDoubleTouch:(UITapGestureRecognizer*) doubleTapTouchGesture {
+    self.imageView.center = self.view.center;
+    [self.imageView.layer removeAllAnimations];
+}
+// поворот по часовой стрелке
+-(void) handleRighSwipeGesture:(UISwipeGestureRecognizer*) rightSwipeGesture {
+    [self rotateView:self.imageView withRotationDuration:1 andAngle:3.14];
     
-
 }
 
+-(void) rotateView:(UIView*) view withRotationDuration:(int) duration andAngle:(CGFloat) angle {
+    CGAffineTransform currentTransform = view.transform;
+    CGAffineTransform newTransform = CGAffineTransformRotate(currentTransform, angle);;
+    
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionBeginFromCurrentState
+                     animations:^{
+                         view.transform = newTransform;
+                     } completion:^(BOOL finished) {
+                         [UIView animateWithDuration:duration
+                                               delay:0
+                                             options:UIViewAnimationOptionCurveLinear | UIViewAnimationOptionBeginFromCurrentState
+                                          animations:^{
+                                              view.transform =CGAffineTransformRotate(currentTransform, angle);
+                                          } completion:^(BOOL finished) {
+                                              
+                                          }];
+                     }];
+    
+}
+// поддержка всех жестов
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
     return YES;
 }
